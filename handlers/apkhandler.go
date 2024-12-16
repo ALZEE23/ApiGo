28f8f79665d16cef2bca40bde9c39cf53fd45a41
ALZEE23 <muhamadalfarizial726@gmail.com>
2024-12-16 10:43:24 +0700

    fix: storage logic

--- handlers/apkhandler.go
+++ handlers/apkhandler.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"net/http"
+	"path/filepath"
 
 	"github.com/ALZEE23/ApiGo/database"
 	"github.com/ALZEE23/ApiGo/models"
@@ -9,12 +10,46 @@ import (
 )
 
 func Apk(context *gin.Context) {
+	_, err := context.MultipartForm()
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Failed to parse form data"})
+		return
+	}
+
+	footageHeader, err := context.FormFile("footage")
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Failed to get footage file"})
+		return
+	}
+
+	coverHeader, err := context.FormFile("cover")
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Failed to get cover file"})
+		return
+	}
+
+	footagePath := filepath.Join("storage", footageHeader.Filename)
+	if err := context.SaveUploadedFile(footageHeader, footagePath); err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save footage file"})
+		return
+	}
+
+	coverPath := filepath.Join("storage", coverHeader.Filename)
+	if err := context.SaveUploadedFile(coverHeader, coverPath); err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save cover file"})
+		return
+	}
+
 	var apk models.Apk
 	if err := context.ShouldBindJSON(&apk); err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		context.Abort()
 		return
 	}
+
+	apk.Footage = footagePath
+	apk.Cover = coverPath
+
 	record := database.DB.Db.Create(&apk)
 	if record.Error != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"error": record.Error.Error()})
